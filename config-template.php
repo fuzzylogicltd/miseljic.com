fe7f6f4824b65c9144886c977b54419abe6e9da0
Lazar <bezpanike@gmail.com>
2021-07-05 14:47:07 -0400

    Removing config file

---
+++ config-template.php
@@ -0,0 +1,9 @@
+<?php
+
+$GLOBALS['base_url'] = '';                  // base URL for all API requests
+$GLOBALS['photo_url'] = '';                 // base URL for fetching images
+$GLOBALS['auth_token'] = '';                // Directus user token
+
+$GLOBALS['default_album'] = '';             // album to show if there's no querystring (ie, this will be home)
+
+?>
--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$GLOBALS['base_url'] = 'https://api.miseljic.com/api';      // base URL for all API requests
-$GLOBALS['photo_url'] = 'https://api.miseljic.com';         // base URL for fetching images
-$GLOBALS['auth_token'] = 'FKTIO3TG62FKRTO567FKWX2';         // Directus user token
-
-$GLOBALS['default_album'] = 'street';                       // album to show if there's no querystring (ie, this will be home)
-
-?>
