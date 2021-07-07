d6343beea958781d15a44b17b906528b561266ba
Lazar <bezpanike@gmail.com>
2021-07-07 06:41:20 -0400

    Better error handling for API calls

--- functions.php
+++ functions.php
@@ -15,10 +15,8 @@ function GetAlbums() {
 
     if (!empty($albums['data'])) { 
         foreach ($albums['data'] as $album) {
-
             $newAlbum = array($album['slug'], $album['name']);
             array_push($GLOBALS['arrAlbums'], $newAlbum);
-            
         }
     }
 }
@@ -29,15 +27,13 @@ function GetStories() {
 
     if (!empty($stories['data'])) { 
         foreach ($stories['data'] as $story) {
-
             $newAlbum = array($story['slug'], $story['name']);
             array_push($GLOBALS['arrStories'], $newAlbum);
-            
         }
     }
 }
 
-function CallAPI($method, $query, $data = false) {
+function CallAPI($method, $query, $data = null) {
 
     $url = $GLOBALS['base_url'] . $query;
     $curl = curl_init();
@@ -63,10 +59,10 @@ function CallAPI($method, $query, $data = false) {
     curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
-    try {
-        $result = curl_exec($curl);
-    } catch (Exception $e) {
-        $result = "Error contacting API: " . $e;
+    $result = curl_exec($curl);
+
+    if(curl_errno($curl)){
+        echo "Error contacting API: " . curl_error($curl);
     }
     
     curl_close($curl);
